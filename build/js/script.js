37d7b37cad2cd8682ade3256d5cc0ffc13661877
Lednev <iLednevii@gmail.com>
2019-08-26 21:09:33 +0300

    Сделан попап, подправлен pixel perfect

--- build/js/script.js
+++ build/js/script.js
@@ -9,3 +9,26 @@ buttons.forEach(function (item, index) {
     lists[index].classList.toggle('closed');
   });
 });
+
+function openPopup() {
+  popup.classList.add('opened');
+  popupCloseButton.addEventListener('click', closePopup);
+  popup.addEventListener('click', closePopup);
+  document.addEventListener('keydown', closePopup);
+}
+
+function closePopup(evt) {
+  var target = evt.target;
+  if (target.classList.contains('js-popup') || target.classList.contains('js-popup-close') || evt.code === 'Escape') {
+    popup.classList.remove('opened');
+  }
+  popupCloseButton.removeEventListener('click', closePopup);
+  popup.removeEventListener('click', closePopup);
+  document.removeEventListener('keydown', closePopup);
+}
+
+var popup = document.querySelector('.js-popup');
+var popupOpenButton = document.querySelector('.js-popup-open');
+var popupCloseButton = document.querySelector('.js-popup-close');
+
+popupOpenButton.addEventListener('click', openPopup)
