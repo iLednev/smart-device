5aa54e9c2298bf6430f0b7e29f39173a7c5de22b
Lednev <iLednevii@gmail.com>
2019-08-28 21:44:42 +0300

    Сделана валидация форм

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Открывает и закрывает разделы в футере
+
 var buttons = document.querySelectorAll('.js-check-button');
 var lists = document.querySelectorAll('.js-check-list');
 
@@ -9,6 +11,8 @@ buttons.forEach(function (item, index) {
     lists[index].classList.toggle('closed');
   });
 });
+
+// Попап обратного звонка
 
 function openPopup() {
   popup.classList.add('opened');
@@ -35,6 +39,8 @@ var popupOpenButton = document.querySelector('.js-popup-open');
 var popupCloseButton = document.querySelector('.js-popup-close');
 
 popupOpenButton.addEventListener('click', openPopup);
+
+// Кнопка "скролл вниз", скроллит к следующему разделу
 
 function scroll(scrollTarget) {
   var scroller = setInterval(function () {
@@ -70,4 +76,73 @@ document.addEventListener('scroll', function () {
   } else if (scrollY < coords.services && scrollButton.classList.contains('hidden')) {
     scrollButton.classList.remove('hidden');
   }
+});
+
+// Валидация полей
+
+var inputsName = document.querySelectorAll('.js-input-name');
+var inputsTel = document.querySelectorAll('.js-input-tel');
+var textareas = document.querySelectorAll('.js-textarea');
+
+var nameField = {
+  inputs: inputsName,
+  check: /[^A-zА-я'\s]/,
+  string: 'Имя не должно содержать цифр и спецсимволов'
+};
+
+var telField = {
+  inputs: inputsTel,
+  check: /[^0-9()\s]/,
+  string: 'Телефон должен содержать только цифры'
+};
+
+var textarea = {
+  inputs: textareas,
+  check: /[^A-zА-я'"0-9\s]/,
+  string: 'Вопрос не должен содержать спецсимволов'
+};
+
+function checkInputs(inputsArr, regExp, errorString) {
+  inputsArr.forEach(function (item) {
+    item.addEventListener('input', function () {
+      if (regExp.test(item.value)) {
+        item.setCustomValidity(errorString);
+      } else {
+        item.setCustomValidity('');
+      }
+    });
+  });
+}
+
+checkInputs(nameField.inputs, nameField.check, nameField.string);
+checkInputs(telField.inputs, telField.check, telField.string);
+checkInputs(textarea.inputs, textarea.check, textarea.string);
+
+
+inputsTel.forEach(function (item) {
+  item.addEventListener('focus', function () {
+    if (!item.value) {
+      item.value = '+7 ';
+    }
+  });
+
+  item.addEventListener('keydown', function (evt) {
+    if (evt.code !== 'Backspace') {
+      if (item.value.length === 3) {
+        evt.preventDefault();
+        item.value += '(' + evt.key;
+      } else if (item.value.length === 6) {
+        evt.preventDefault();
+        item.value += evt.key + ') ';
+      } else if (item.value.length === 11) {
+        evt.preventDefault();
+        item.value += evt.key + ' ';
+      } else if (item.value.length === 14) {
+        evt.preventDefault();
+        item.value += evt.key + '-';
+      } else if (item.value.length === 18) {
+        evt.preventDefault();
+      }
+    }
+  });
 });
