cbd63cb88f71d73f0403459bb9fb38a41c889f57
Lednev <iLednevii@gmail.com>
2019-08-28 07:34:48 +0300

    Исправлено большинство замечаний

--- source/js/script.js
+++ source/js/script.js
@@ -15,20 +15,59 @@ function openPopup() {
   popupCloseButton.addEventListener('click', closePopup);
   popup.addEventListener('click', closePopup);
   document.addEventListener('keydown', closePopup);
+  page.classList.add('popup-opened');
 }
 
 function closePopup(evt) {
   var target = evt.target;
   if (target.classList.contains('js-popup') || target.classList.contains('js-popup-close') || evt.code === 'Escape') {
     popup.classList.remove('opened');
+    popupCloseButton.removeEventListener('click', closePopup);
+    popup.removeEventListener('click', closePopup);
+    document.removeEventListener('keydown', closePopup);
+    page.classList.remove('popup-opened');
   }
-  popupCloseButton.removeEventListener('click', closePopup);
-  popup.removeEventListener('click', closePopup);
-  document.removeEventListener('keydown', closePopup);
 }
 
+var page = document.querySelector('.page');
 var popup = document.querySelector('.js-popup');
 var popupOpenButton = document.querySelector('.js-popup-open');
 var popupCloseButton = document.querySelector('.js-popup-close');
 
-popupOpenButton.addEventListener('click', openPopup)
+popupOpenButton.addEventListener('click', openPopup);
+
+function scroll(scrollTarget) {
+  var scroller = setInterval(function () {
+    if (scrollY < scrollTarget) {
+      window.scrollBy(0, 20);
+    } else {
+      clearInterval(scroller);
+    }
+  }, 10);
+}
+
+var coords = {
+  advantages: document.querySelector('.advantages').offsetTop,
+  aboutCompany: document.querySelector('.about-company').offsetTop,
+  services: document.querySelector('.services').offsetTop,
+};
+
+var scrollButton = document.querySelector('.js-scroll');
+
+scrollButton.addEventListener('click', function () {
+  if (scrollY < coords.advantages) {
+    scroll(coords.advantages);
+  } else if (scrollY >= coords.advantages && scrollY < coords.aboutCompany) {
+    scroll(coords.aboutCompany);
+  } else if (scrollY >= coords.aboutCompany && scrollY < coords.services) {
+    scroll(coords.services);
+  }
+});
+
+document.addEventListener('scroll', function () {
+  if (scrollY >= coords.services) {
+    scrollButton.classList.add('hidden');
+  } else if (scrollY < coords.services && scrollButton.classList.contains('hidden')) {
+    scrollButton.classList.remove('hidden');
+  }
+});
